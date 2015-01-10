27404650d28ae19a1a19a7d90f8dc33e4d9111f6
Andrew Fischer <afischer15@mac.com>
2015-01-10 15:05:49 -0500

    now sends successfully to raspi

--- arduino/SendString/SendString.ino
+++ arduino/SendString/SendString.ino
@@ -8,7 +8,18 @@
 
 //////Declarations
 /// Globals
-int msg[1];
+int serial_putc( char c, FILE * ) 
+{
+  Serial.write(c);
+  return c;
+} 
+void printf_begin(void)
+{
+  fdevopen( &serial_putc, 0 );
+}
+
+char SendPayload[30] = "init";
+
 int val = 0;
 unsigned long lastMove1 = 0;
 unsigned long lastMove2 = 0;
@@ -17,7 +28,9 @@ boolean on2 = false;
 
 /// Radios
 RF24 radio(9,10);
-const uint64_t pipe = 0xE8E8F0F0E1LL; //Out Pipe
+//we only need a write pipe, but I made two anyway
+const uint64_t pipe[2] = { 
+  0xF0F0F0F0E1LL,0xF0F0F0F0D2LL };
 
 /// Rotary Encoders
 RotaryEncoder encoder1(A0,A1,5,6,3000);
@@ -26,11 +39,22 @@ RotaryEncoder encoder2(A4,A5,5,6,3000);
 
 ///////////////////////////////////////////////////
 
+
 void setup(void){
   Serial.begin(57600);
+  printf_begin();
+  //RF24 Config
   radio.begin();
-  radio.openWritingPipe(pipe);
-  
+  radio.setChannel(0x4c);
+  radio.setAutoAck(1);
+  radio.setRetries(15,15);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPayloadSize(32);
+  radio.openReadingPipe(1,pipe[0]);
+  radio.openWritingPipe(pipe[1]);
+  radio.startListening();
+  radio.printDetails(); //for Debugging
+
   //Set pin 7 as a faux ground, b/c the other 2 are used.
   pinMode(7,OUTPUT);
   digitalWrite(7,LOW);
@@ -43,64 +67,64 @@ void loop()
   //Serial.println(millis());
   int enc1 = encoder1.readEncoder();
   int enc2 = encoder2.readEncoder();
-  
+
   int changevalue = 1;
   if(enc1) {
     lastMove1 = millis();
     on1 = true;    
     Serial.println("Encoder 1 Moved!");
   } 
-  
-   if(enc2) {
+
+  if(enc2) {
     lastMove2 = millis();
     on2 = true;
     Serial.println("Encoder 2 Moved!");
   } 
-  
-  if ((millis() - lastMove1) > 5000 && on1){
-      on1 = false;
-      Serial.print("Encoder 1 went idle. Offline for");
-      Serial.println(millis() - lastMove1);
-     // Serial.println(lastMove1 - millis()); 
+
+  if ((millis() - lastMove1) > 10000 && on1){
+    on1 = false;
+    Serial.print("Encoder 1 went idle. Offline for");
+    Serial.println(millis() - lastMove1);
+    // Serial.println(lastMove1 - millis()); 
   }
-  
-  if ((millis() - lastMove2) > 5000 && on2){
-      on2 = false;
-      Serial.print("Encoder 2 went idle. Offline for:");
-      Serial.println(millis() - lastMove2);
-     // Serial.println(lastMove1 - millis()); 
+
+  if ((millis() - lastMove2) > 10000 && on2){
+    on2 = false;
+    Serial.print("Encoder 2 went idle. Offline for:");
+    Serial.println(millis() - lastMove2);
+    // Serial.println(lastMove1 - millis()); 
   }
-  
+
   if (millis()%10000 == 0){
-    // DATA TO SEND GO HERE
     // Escilator is 4 for up and 5 down for 5, send 
-     Serial.println("Sending data...");
-     String data = "{4: " + String(on1) + ", 5: " + String(on2) + "}" ;
-     sendMsg(data);
-     Serial.println(data);
-     delayMicroseconds(50);
+    Serial.println("Sending data...");
+    String data = "{4: " + String(on1) + ", 5: " + String(on2) + "}";
+    data.toCharArray(SendPayload, data.length()+1);
+
+   // Serial.print("SendPayload[1] = ");
+  //  Serial.println(SendPayload[1]);
+    sendMsg(data);
+    delayMicroseconds(50);
 
   }
-  
+
   delayMicroseconds(50);
 }
 
 
-void sendMsg(String sendStr){
-  String message = sendStr;
-  int messageSize = message.length();
-  for (int i = 0; i < messageSize; i++) {
-    int charToSend[1];
-    charToSend[0] = message.charAt(i);
-    radio.write(charToSend,1);
-  }  
-  //send the 'terminate string' value...  
-  msg[0] = 2; 
-  radio.write(msg,1);
+void sendMsg(String data){
+   //Get payload from sensor
 
-  radio.powerDown(); 
-  delay(1000);
-  radio.powerUp();
-  Serial.println("Sent String Successfully. Contents: " + message);
+  //toAscii();
+
+  //send out message
+  radio.stopListening();
+  bool ok = radio.write(&SendPayload,strlen(SendPayload));
+  radio.startListening(); 
+
+  // Send every 2 secs
+
+  Serial.println("Sent String Successfully. Contents: " + data);
 }
+
 
