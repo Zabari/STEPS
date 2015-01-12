89878c146b44610a2ba6863dbcfb75fd84559c02
Zabari <zabari.daniel@Gmail.com>
2015-01-12 12:13:42 -0500

    fixed parameters or whatever

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ I'll assume module is called `server` and has methods:
          value is dictionary of data. "Status" key contains status
          of each escalator, and we can easily add more data fields
          to each escalator.
-    fetchData()
+    fetchData(EscalatorName)
+          returns nested dictionary with single key Escalator Name, and then
+          "status or whatever.
+    fetchAll()
         returns same format as above: Dictionary<String:Dictionary>
+        x = { "4-6": {"status":"running"}}  x{"4-6":}
 '''
 
 app = Flask(__name__)
