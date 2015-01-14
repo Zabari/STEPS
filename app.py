4b48fe70c6a20c0a96accaba0cf26972a607b9fb
Andrew Fischer <afischer15@mac.com>
2015-01-13 22:25:56 -0500

    new UI stuff

--- app.py
+++ app.py
@@ -2,19 +2,33 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+@app.route("/status")
 @app.route("/home")
 @app.route("/")
-def home():
-    return render_template("test.html");
-
-@app.route("/status")
 def status():
-    return render_template("status.html");
+    return render_template("status.html", title="STEPS");
+
+@app.route("/base")
+def base():
+    return render_template("base.html", title="STEPS");
 
 
-@app.route("/bootstatus")
-def bootstatus():
-    return render_template("bootstatus.html");
+@app.route("/about")
+def about():
+    return render_template("about.html", title="STEPS");
+
+@app.route("/api")
+def api():
+    return render_template("api.html", title="STEPS");
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="STEPS");
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html", title="STEPS");
+
 
 if __name__=="__main__":
     app.debug=True
--- static/js/script.js
+++ static/js/script.js
@@ -67,3 +67,36 @@ function setRandom() {
 window.setInterval(function(){
   reloadData();
 }, 2000);
+
+
+
+// SIDEBAR
+/* activate sidebar */
+$('#sidebar').affix({
+  offset: {
+    top: 235
+  }
+});
+
+/* activate scrollspy menu */
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+  target: '#leftCol',
+  offset: navHeight
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 60
+      }, 500);
+      return false;
+    }
+  }
+});
