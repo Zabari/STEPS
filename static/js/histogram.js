c212b6734012b27c07e88d0ce54289e592c4ca6e
Aaron Mortenson <aamortenson1@gmail.com>
2015-01-15 11:03:47 -0500

    added some text and scaling and stuff

--- static/js/histogram.js
+++ static/js/histogram.js
@@ -2,6 +2,7 @@ var inc = (tNow-tFirst)/(window.innerWidth*.8)
 var time = tNow
 var activeTracker = act.slice()
 var graphs = []
+var names = ["2-3","2-4","3-5","4-6","5-7","6-8","7-9"]
 for (var n = 0; n < hist.length; n++) {
     graphs[graphs.length] = [0]
 }
@@ -17,18 +18,45 @@ while (time >= tFirst) {
 	time -= inc;
 }
 var svgContainer = d3.select("body").append("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", Math.abs(window.innerHeight - 100));
+    .attr("width", window.innerWidth-10)
+    .attr("height", Math.abs(window.innerHeight-10));
 
 for (var n = 0; n < graphs.length; n++) {
     var l = graphs[n].length
     var positioner = 0
+    var arrow = svgContainer.append("text")
+	.attr("x",(window.innerHeight-65)*.18 + 10 + window.innerWidth*.8)
+	.attr("y",5*(n+1)+10+(window.innerHeight-65)*.06*(n+1))
+	.attr("dy","0em")
+	.style("font-size",(window.innerHeight-65)*.1+"px")
+	.style("font-weight", "bold")
+	.style("text-anchor","start");
+    if (n%2 == 0) {
+	arrow.text("↗");
+	var text = svgContainer.append("text")
+	    .attr("x",10)
+	    .attr("y",5*(n+1)+10+(window.innerHeight-65)*.06*(n+1))
+	    .attr("dy",".35em")
+	    .style("font-size",(window.innerHeight-65)*.1+"px")
+	    .style("text-anchor","start")
+	    .text(names[n/2]);
+    }
+    else
+	arrow.text("↘");
+    
+    if (act[n]) {
+	arrow.style("fill","green")
+    }
+    else {
+	arrow.style("fill","red")
+    }
+	    
     for (var g = l-1; g >= 0; g--) {
 	var rect = svgContainer.append("rect")
-	    .attr("x",10+positioner)
-	    .attr("y",5*n+10+window.innerHeight*.05*n)
+	    .attr("x",(window.innerHeight-65)*.18 + 10 + positioner)
+	    .attr("y",5*n+10+(window.innerHeight-65)*.06*n)
 	    .attr("width", graphs[n][g])
-	    .attr("height", window.innerHeight*.05);
+	    .attr("height", (window.innerHeight-65)*.06);
 	if (activeTracker[n] == l%2)
 	    rect.attr("fill", "green")
 	else
