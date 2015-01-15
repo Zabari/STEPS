a085074749ccd340b6ef028c1ca0eaac6ebcbfe9
Zabari <zabari.daniel@Gmail.com>
2015-01-15 12:35:51 -0500

    breaking everything

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ false=False
 d={'data':[[6, false, 6421203495.845522],[6, true, 5421203495.845522],[13, true, 4421203495.845522],[12, false, 3421203495.845522],[10, false, 2421203495.845522],[3, true, 1421203495.845522],[8, true, 1421203492.84195],[5, false, 1421203498.848851],[0, true, 1421203500.851109],[2, true, 1421203489.83835], [9, true, 1421203489.838356],\
 [4,true ,40432523.4324], [1, true, 1421203490.83955],[7, true, 1421203490.839556], [11, true, 1421203490.839558]]} #for testing
 def updateDatabase(data):
-    conn = sqlite3.connect('/var/www/FlaskApp/EscalatorHaus/esc.db')
+    conn = sqlite3.connect('esc.db')
     c=conn.cursor()
     try:
         c.execute('CREATE TABLE esc (name TEXT,time REAL, status BOOLEAN)')
@@ -20,17 +20,29 @@ def updateDatabase(data):
 
 def fetchAll():
     d={}
-    conn = sqlite3.connect('/var/www/FlaskApp/EscalatorHaus/esc.db')
+    conn = sqlite3.connect('esc.db')
     c=conn.cursor()
     for x in L:
         d[x]={}
-        d[x]['status']=(c.execute('SELECT * FROM esc WHERE name=? ORDER BY time',(x,)).fetchall()[-1][-1]==1)
+        maxi=c.execute('SELECT max(time) FROM esc where name=?',(x,)).fetchall()[0][0]
+        #print maxi
+        d[x]['status']=(c.execute('SELECT status FROM esc WHERE name=? AND time=?',(x,maxi,)).fetchall()[-1][-1]==1)
+        #d[x]['status']=(c.execute('SELECT * FROM esc WHERE name=? ORDER BY time',(x,)).fetchall()[-1][-1]==1)
         #print d[x]['status']
     conn.close()
     return d
-#updateDatabase(d) #for testing
+def fetchTime(esci):
+    ret=[]
+    conn = sqlite3.connect('esc.db')
+    c=conn.cursor()
+    ret=c.execute('SELECT * FROM esc WHERE name=? ORDER BY time',(L[esci],)).fetchAll()
+    return ret
+    conn.close()
+#def fetchAllTime():
+updateDatabase(d) #for testing
 #conn = sqlite3.connect('esc.db')
 #c=conn.cursor()
 #print c.execute('SELECT * FROM esc WHERE name=?',("up46",)).fetchall()
 #conn.close()
-#print fetchAll() #for testing
+print fetchAll() #for testing
+print fetchTime(0)
