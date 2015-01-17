552d1eb5ba6949749d3ca6f844b9ffd46000088a
Andrew Fischer <afischer15@mac.com>
2015-01-17 12:09:39 -0500

    Merging histogram, new error pages,

--- __init__.py
+++ __init__.py
@@ -1,32 +1,56 @@
 from flask import Flask,render_template
+import json, server, time
 
 app = Flask(__name__)
+
+###### Error Handlers ######
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+
+######     Routes     ######
 
 @app.route("/status")
 @app.route("/home")
 @app.route("/")
 def status():
-    return render_template("status.html");
-
-@app.route("/about")
-def about():
-    return render_template("about.html");
-
-@app.route("/api")
-def api():
-    return render_template("api.html");
-
-@app.route("/stats")
-def stats():
-    return render_template("stats.html");
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html");
+    return render_template("status.html", title="STEPS");
 
 @app.route("/base")
 def base():
-    return render_template("base.html");
+    return render_template("base.html", title="STEPS");
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="STEPS");
+
+@app.route("/api")
+def api():
+    return render_template("api.html", title="STEPS");
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="STEPS");
+
+@app.route("/stats")
+def histogram():
+    active = server.fetchStatusAll()
+    history = server.fetchAllTime()
+
+    tCurrent = time.time()
+    tFirst = tCurrent
+    trackers = []
+
+    for esc in history:
+        trackers.append(len(esc)-1)
+        if len(esc) > 0 and esc[0] < tFirst:
+            tFirst = esc[0]
+    return render_template("histogram.html", history = history, active = active, tCurrent = tCurrent, tFirst = tFirst, trackers = trackers)
+
 
 
 if __name__=="__main__":
---
+++ server.py
@@ -0,0 +1,68 @@
+import sqlite3
+L = ["up23", "down23","up24", "down24","up35", "down35","up46", "down46","up57", "down57","up68", "down68","up79", "down79"]
+true=True
+false=False
+d={'data':[[6, false, 6421203495.845522],[6, true, 5421203495.845522],[13, true, 4421203495.845522],[12, false, 3421203495.845522],[10, false, 2421203495.845522],[3, true, 1421203495.845522],[8, true, 1421203492.84195],[5, false, 1421203498.848851],[0, true, 1421203500.851109],[2, true, 1421203489.83835], [9, true, 1421203489.838356],\
+[4,true ,40432523.4324], [1, true, 1421203490.83955],[7, true, 1421203490.839556], [11, true, 1421203490.839558]]} #for testing
+def updateDatabase(data):
+    conn = sqlite3.connect('/var/www/FlaskApp/STEPS/esc.db')
+    c=conn.cursor()
+    try:
+        c.execute('CREATE TABLE esc (name TEXT,time REAL, status BOOLEAN)')
+    except:
+        print
+    for x in data["data"]:
+        tup=(L[x[0]],x[2],x[1])
+        #print tup
+        c.execute('INSERT INTO esc VALUES (?,?,?)',tup)
+        conn.commit()
+    conn.close()
+
+def fetchAll():
+    d={}
+    conn = sqlite3.connect('/var/www/FlaskApp/STEPS/esc.db')
+    c=conn.cursor()
+    for x in L:
+        d[x]={}
+        maxi=c.execute('SELECT max(time) FROM esc where name=?',(x,)).fetchall()[0][0]
+        #print maxi
+        d[x]['status']=(c.execute('SELECT status FROM esc WHERE name=? AND time=?',(x,maxi,)).fetchall()[-1][-1]==1)
+        #d[x]['status']=(c.execute('SELECT * FROM esc WHERE name=? ORDER BY time',(x,)).fetchall()[-1][-1]==1)
+        #print d[x]['status']
+    conn.close()
+    return d
+def fetchTime(esci):
+    ret=[]
+    conn = sqlite3.connect('/var/www/FlaskApp/STEPS/esc.db')
+    c=conn.cursor()
+    ret=c.execute('SELECT time FROM esc WHERE name=? ORDER BY time',(L[esci],)).fetchall()
+    for i in range(len(ret)):
+        ret[i]=ret[i][0]
+    conn.close()
+    return ret
+def fetchAllTime():
+    ret=[]
+    for i in range(len(L)):
+        ret.append(fetchTime(i))
+    return ret
+def fetchStatus(esci):
+    ret=[]
+    conn = sqlite3.connect('/var/www/FlaskApp/STEPS/esc.db')
+    c=conn.cursor()
+    maxi=c.execute('SELECT max(time) FROM esc where name=?',(L[esci],)).fetchall()[0][0]
+    ret=c.execute('SELECT status FROM esc WHERE name=? AND time=?',(L[esci],maxi,)).fetchall()[0][0]==1
+    conn.close()
+    return ret
+def fetchStatusAll():
+    ret=[]
+    for i in range(len(L)):
+        ret.append(fetchStatus(i))
+    return ret
+#updateDatabase(d) #for testing
+#conn = sqlite3.connect('esc.db')
+#c=conn.cursor()
+#print c.execute('SELECT * FROM esc WHERE name=?',("up46",)).fetchall()
+#conn.close()
+#print fetchAll() #for testing
+#print fetchAllTime()
+#print fetchStatusAll()
