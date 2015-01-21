756e41563314798e4d2b84cadf74b07703f483e2
Andrew Fischer <afischer15@mac.com>
2015-01-20 19:24:13 -0500

    error handlers

--- __init__.py
+++ __init__.py
@@ -49,10 +49,15 @@ api = restful.Api(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errorHandlers/404.html'), 404
 
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('errorHandlers/405.html'), 405
 
-
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errorHandlers/500.html'), 500
 
 ######## Routes #########
 @app.route("/status")
